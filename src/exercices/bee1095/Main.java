00385ff635ce28bf73071ffcb6173817eafb8851
GabrielCrespo <gabriel.crespo1942@gmail.com>
2023-09-11 22:04:08 -0300

    bee1095

---
+++ src/exercices/bee1095/Main.java
@@ -0,0 +1,18 @@
+package exercices.bee1095;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int i = 1;
+
+        for(int j = 60; j >= 0; j=j-5) {
+            System.out.println("I=" + i + " " + "J=" + j);
+            i+=3;
+        }
+
+    }
+
+}
