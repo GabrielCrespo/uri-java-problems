8002ab3357e5fc641642b68939f18e0e58e78720
GabrielCrespo <gabriel.crespo1942@gmail.com>
2023-09-27 23:54:02 -0300

    bee1145

---
+++ src/exercices/be1145/Main.java
@@ -0,0 +1,28 @@
+package exercices.be1145;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int quantidadeValoresPorLinha = scanner.nextInt();
+        int quantidadeValores = scanner.nextInt();
+
+        for (int i = 1; i <= quantidadeValores; i++) {
+
+            if(i % quantidadeValoresPorLinha == 0) {
+                System.out.println(i);
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+
+    }
+
+}
