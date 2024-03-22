14bfc1ed080642fcd940b644a636461b62465fec
GabrielCrespo <gabriel.crespo1942@gmail.com>
2024-03-22 17:16:07 -0300

    bee1959

---
+++ src/bee1959/Main.java
@@ -0,0 +1,23 @@
+package bee1959;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long numberOfSides = scanner.nextInt();
+        long sizeOfEachSide = scanner.nextInt();
+
+        long perimeter = numberOfSides * sizeOfEachSide;
+
+        System.out.println(perimeter);
+
+        scanner.close();
+
+    }
+
+}
