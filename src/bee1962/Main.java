b6b3c3f0dad028604be4965fd77439b63289c7c3
GabrielCrespo <gabriel.crespo1942@gmail.com>
2024-03-24 16:16:48 -0300

    bee1962

---
+++ src/bee1962/Main.java
@@ -0,0 +1,56 @@
+package bee1962;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String BEFORE_CHRIST = "A.C.";
+    public static final String AFTER_CHRIST = "D.C.";
+    public static final long BASE_YEAR = 2015;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfLines = scanner.nextInt();
+
+        long year = 0;
+
+        for (int i = 0; i < numberOfLines; i++) {
+
+            year = scanner.nextLong();
+
+            String result = getCorrespondingYear(year);
+
+            System.out.println(result);
+
+        }
+
+        scanner.close();
+
+    }
+
+    private static String getCorrespondingYear(long year) {
+
+        StringBuilder builder = new StringBuilder();
+
+        long result;
+
+        if (year >= BASE_YEAR) {
+            result = year - BASE_YEAR + 1;
+            builder.append(result)
+                    .append(" ")
+                    .append(BEFORE_CHRIST);
+        } else {
+            result = BASE_YEAR - year;
+            builder.append(result)
+                    .append(" ")
+                    .append(AFTER_CHRIST);
+        }
+
+        return builder.toString();
+
+    }
+
+}
