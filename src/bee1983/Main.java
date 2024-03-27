04799d80677d5336c6c6be436e96303c52c5967e
GabrielCrespo <gabriel.crespo1942@gmail.com>
2024-03-27 00:03:56 -0300

    bee1983

---
+++ src/bee1983/Main.java
@@ -0,0 +1,45 @@
+package bee1983;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String MESSAGE = "Minimum note not reached";
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfStudents = scanner.nextInt();
+        long theChosenOne = 0;
+        double mostGrade = 0.0;
+
+
+        for (int i = 0; i < numberOfStudents; i++) {
+
+            long registration = scanner.nextLong();
+            double grade = scanner.nextDouble();
+
+            if (grade > mostGrade) {
+                mostGrade = grade;
+                theChosenOne = registration;
+            }
+
+        }
+
+        if (mostGrade < 8) {
+            System.out.println(MESSAGE);
+        } else {
+            System.out.println(theChosenOne);
+        }
+
+
+        scanner.close();
+
+
+    }
+
+
+}
